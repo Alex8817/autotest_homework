a629397aaa604117cf40cb45ace79b0117a6f97d
Alex8817 <alex.ivanov0988@gmail.com>
2023-07-07 07:20:15 +0200

    Added sout1

--- src/main/java/NewTest.java
+++ src/main/java/NewTest.java
@@ -5,8 +5,9 @@ import org.junit.jupiter.api.Test;
 public class NewTest {
     @Test
     void newTest() {
-        System.out.println("This is the best test");
+        System.out.println("This is the best test1");
         Assertions.assertTrue(3 > 2);
+
 
 
     }
